d765deef57091b2dba5cd9547917cf0e518aa05a
Ilja Gužovski <ilja.guzovski@gmail.com>
2020-04-21 13:51:35 +0300

    Adjust score processing

--- index.ts
+++ index.ts
@@ -6,25 +6,38 @@ function calculateScore() {
     const state = yahoodb.getState().stocks;
 
     const pe = _.orderBy(state.filter(it => it["Trailing PE"]), ["Trailing PE"], ["desc"]);
+    const ps = _.orderBy(state.filter(it => it["Price/sales"]), ["Price/sales"], ["desc"]);
+    const pb = _.orderBy(state.filter(it => it["Price/book"]), ["Price/book"], ["desc"]);
+
     const om = _.orderBy(state.filter(it => it["Operating Margin (ttm)"]), ["Operating Margin (ttm)"]);
+
     const roa = _.orderBy(state.filter(it => it["Return on Assets"]), ["Return on Assets"]);
     const roe = _.orderBy(state.filter(it => it["Return on Equity"]), ["Return on Equity"]);
+
     const qrg = _.orderBy(state.filter(it => it["Quarterly revenue growth (yoy)"]), ["Quarterly revenue growth (yoy)"]);
     const qeg = _.orderBy(state.filter(it => it["Quarterly earnings growth (yoy)"]), ["Quarterly earnings growth (yoy)"]);
+
     const de = _.orderBy(state.filter(it => it["Total debt/equity (mrq)"]), ["Total debt/equity (mrq)"], ["desc"]);
     const tady = _.orderBy(state.filter(it => it["Trailing Annual Dividend Yield"]), ["Trailing Annual Dividend Yield"]);
+    const payr = _.orderBy(state.filter(it => it["Payout ratio"]), ["Payout ratio"], ["desc"]);
+
+    const change52 = _.orderBy(state.filter(it => it["52-week change"]), ["52-week change"], ["desc"]);
 
     const scored = state.map(it => ({
         ...it,
         score:
-            pe.findIndex(that => that.ticker === it.ticker) +
+            0.5 * pe.findIndex(that => that.ticker === it.ticker) +
+            0.5 * ps.findIndex(that => that.ticker === it.ticker) +
+            pb.findIndex(that => that.ticker === it.ticker) +
             om.findIndex(that => that.ticker === it.ticker) +
-            roa.findIndex(that => that.ticker === it.ticker) +
-            roe.findIndex(that => that.ticker === it.ticker)+
-            qrg.findIndex(that => that.ticker === it.ticker)+
-            qeg.findIndex(that => that.ticker === it.ticker)+
-            de.findIndex(that => that.ticker === it.ticker)+
-            tady.findIndex(that => that.ticker === it.ticker)
+            2 * roa.findIndex(that => that.ticker === it.ticker) +
+            roe.findIndex(that => that.ticker === it.ticker) +
+            2 * qrg.findIndex(that => that.ticker === it.ticker) +
+            qeg.findIndex(that => that.ticker === it.ticker) +
+            de.findIndex(that => that.ticker === it.ticker) +
+            2 * tady.findIndex(that => that.ticker === it.ticker)+
+            payr.findIndex(that => that.ticker === it.ticker) +
+            change52.findIndex(that => that.ticker === it.ticker)
     }));
     const scoredSorted = _.orderBy(scored, ["score"], ["desc"]);
     scoredb.set('stocks', scoredSorted).write();
